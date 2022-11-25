f8154ac9895df2f0fed30822ca5bc04237a2284b
tmelliottjr <tmelliottjr@gmail.com>
2022-11-25 12:03:28 -0500

    feat: initial version

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { Octokit } from "@octokit/core";
+import { VERSION } from "./version";
+
+type Options = Record<string, unknown>;
+
+/**
+ * @param octokit Octokit instance
+ * @param options Options passed to Octokit constructor
+ */
+export function test(octokit: Octokit, options: Options) {}
+test.VERSION = VERSION;
